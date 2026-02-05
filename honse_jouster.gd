ec0e8625b2e535dc38a5251fb0a00d74cdfe688a
utinj <Utinj123@gmail.com>
2026-02-04 20:55:14 -0500

    Model adjustments

--- honse_jouster.gd
+++ honse_jouster.gd
@@ -4,5 +4,6 @@ extends Node3D
 @export var honse: Skeleton3D
 
 func _ready():
-	knight.get_child(1).physical_bones_start_simulation()
-	honse.get_child(1).physical_bones_start_simulation()
+	print("Umamusume!")
+	#knight.get_child(1).physical_bones_start_simulation()
+	#honse.get_child(1).physical_bones_start_simulation()
